d9a708fdb61d3cc5862efaa6a73d6ac3eb62bfd0
GerryWilko <gerard.wilkinson@me.com>
2022-11-09 10:26:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-fetch-typing-issue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-ksmmzs)
---
+++ app.vue
@@ -0,0 +1,5 @@
+<script setup>
+const { data } = await useAsyncData('count', () => $fetch('/counter'));
+</script>
+
+<template>Page visits: {{ data }}</template>
