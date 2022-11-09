a72b403c867586b21e0c149108104d1935ab248b
Gerard Wilkinson <gerard@thinkactivelabs.co.uk>
2022-11-09 10:38:59 +0000

    Complete repro

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
-<script setup>
-const { data } = await useAsyncData('count', () => $fetch('/counter'));
+<script setup lang="ts">
+// both are typed on initial install
+// run npm run generate and then typings conflict with something in dist folder on anything using $fetch (i.e. second one)
+const { data: typed } = useFetch('/api/counter')
+const { data: notTyped } = useAsyncData('count', () => $fetch('/api/counter'));
 </script>
 
-<template>Page visits: {{ data }}</template>
+<template>
+<p>
+    Page visits (typed): {{ typed }}
+</p>
+<p>
+    Page visits (notTyped): {{ notTyped }}
+</p>
+</template>
